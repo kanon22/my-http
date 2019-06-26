c6000795a9a5c2befa27046638858e4cd2eb1ab2
kanon22 <k.sasaki1397@gmail.com>
2019-06-26 18:58:32 +0900

    画像を表示できた

--- serv.c
+++ serv.c
@@ -5,12 +5,14 @@
 #include <netinet/in.h>
 #include <errno.h>
 #include <string.h>
+#include <fcntl.h>
 
 void raise_error(char* description){
 	perror(description);
 	printf("errno: %d\n", errno);
 }
 
+/* strlenしてるからバイナリは送れないよ！！ */
 int write_msg(int sock, char* msg){
 	int len = strlen(msg);
 	if(write(sock, msg, len) != len){
@@ -20,23 +22,69 @@ int write_msg(int sock, char* msg){
 	return len;
 }
 
+int write_file(int sock, char* file_path){
+	int fd, len;
+	char* buf[1024];
+
+	fd = open(file_path, O_RDONLY);
+	if(fd < 0){
+		raise_error("opening file");
+		fprintf(stderr, "cannot open file %s\n", file_path);
+		//write_msg(wsock, "HTTP/1.1 404 Not Found");
+	}else{
+		/* send file */
+		while((len = read(fd, buf, 1024)) > 0){
+			write(sock, buf, len);
+		}
+	}
+
+	close(fd);
+	return len;
+}
+
 int http(int wsock, char* request){
 	char method[16];
 	char uri[256];
 	char version[64];
+	char* file_uri;
+	int fd, len;
+	char buf[1024];
+	char content_type[64];
 
 	sscanf(request, "%s %s %s", method, uri, version);
 	//fprintf(stdout, "%s %s %s\n", method, uri, version);
 	/* GET以外のリクエストをはじく */
 	if (strcmp(method, "GET") != 0) {
-		write_msg(wsock, "HTTP1.1 501 ");
+		write_msg(wsock, "HTTP/1.1 501 ");
 		write_msg(wsock, "Not Implemented");
 	}
 
 	/* send message */
-	write_msg(wsock, "HTTP1.1 200 OK");
-	//write_msg(wsock, "HTTP1.1 302 FOUND\r\n");
+	write_msg(wsock, "HTTP/1.1 200 OK\r\n");
+	//write_msg(wsock, "HTTP/1.1 302 FOUND\r\n");
 	//write_msg(wsock, "Location: https://hikalium.com");
+
+	/* open file with uri */
+	if(strcmp(uri, "/") == 0){
+		file_uri = "index.html";
+	}else{
+		file_uri = uri + 1; // pathの最初の/を取り除く
+	}
+	//write_msg(wsock, "text/html\r\n");
+	/* detect file type */
+	if(strstr(file_uri, "html")){
+		strcpy(content_type, "Content-Type: text/html\r\n");
+	}else if(strstr(file_uri, "png") || strstr(file_uri, "ico")){
+		strcpy(content_type, "Content-Type: image/png\r\n");
+	}else if(strstr(file_uri, "jpg") || strstr(file_uri, "jpeg")){
+		strcpy(content_type, "Content-Type: image/jpeg\r\n");
+	}
+	write_msg(wsock, content_type);
+
+	write_msg(wsock, "\r\n");
+
+	write_file(wsock, file_uri);
+
 	return 0; //ステータスコード返したら楽しそう
 }
 
